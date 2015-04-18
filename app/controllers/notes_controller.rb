e7709148e0e909a57fef46dd93b04988f1a10e4a
Kyle Lortscher <kyle91302@gmail.com>
2015-04-18 08:51:23 -0700

    Added Note Params

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,4 +1,5 @@
 class NotesController < ApplicationController
+	before_action :find_note, only: [:show, :edit, :update, :destroy]
 	def index
 	end
 	
@@ -11,6 +12,12 @@ class NotesController < ApplicationController
 
 	def create
 		@note = Note.new(note_params)
+
+		if @note.save
+			redirect_to @note
+		else
+			render 'new'
+		end		
 	end
 
 	def edit
@@ -25,6 +32,7 @@ class NotesController < ApplicationController
 	private 
 
 	def find_note
+		@note = Note.find(params[:id])
 	end
 
 	def note_params
